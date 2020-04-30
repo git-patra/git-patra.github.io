075617039719116d7bceb379ca997fe80bf6deaa
Patra <git-patra@github.com>
2020-04-30 15:34:59 +0700

    New Portfolio

---
+++ js/main.js
@@ -0,0 +1,33 @@
+AOS.init();
+
+//! Scrollspy
+
+$(document).ready(function () {
+  $(".scrollspy").scrollSpy({
+    scrollOffset: 200,
+  });
+});
+
+//! MaterialBox
+
+const materialbox = document.querySelectorAll(".materialboxed");
+M.Materialbox.init(materialbox);
+
+//! ===== Scroll to Top ====
+$(window).scroll(function () {
+  if ($(this).scrollTop() >= 50) {
+    // If page is scrolled more than 50px
+    $("#return-to-top").fadeIn(200); // Fade in the arrow
+  } else {
+    $("#return-to-top").fadeOut(200); // Else fade out the arrow
+  }
+});
+$("#return-to-top").click(function () {
+  // When arrow is clicked
+  $("body,html").animate(
+    {
+      scrollTop: 0, // Scroll to top of body
+    },
+    400
+  );
+});
